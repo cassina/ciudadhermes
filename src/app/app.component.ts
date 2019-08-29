4a638d8065f83a195c7be5120be8736eec932612
cassina <bernardocassina@gmail.com>
2019-08-29 08:47:19 -0500

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'ciudadhermes';
+export class AppComponent implements OnInit {
+  public cities = [
+    'andeonimbva',
+    'kaschkar',
+    'kgargaria',
+    'st-charles',
+    'tekomoros'
+  ];
+  ngOnInit() {
+    const title = 'ciudadhermes';
+  }
 }
